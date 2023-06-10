16e595d1d9cd1639225634eb059fd78480ea6e44
jayafoods <jayafoods72@gmail.com>
2023-06-09 20:04:11 -0400

    Added Search functionality

--- js/menus.js
+++ js/menus.js
@@ -1,6 +1,76 @@
+// ==========================
+// Search functionality
+// ==========================
+
+document.getElementById('search-input').addEventListener('keyup', function() {
+  let searchQuery = this.value.toLowerCase();
+
+  let items = document.querySelectorAll('.text-menu .item-list .item');
+
+  const searchResultsContainer = document.getElementById('search-results');
+  const searchResultsItemsContainer = document.getElementById('search-results-items');
+
+  // Clear the search results container before appending new content
+  while (searchResultsItemsContainer.firstChild) {
+    searchResultsItemsContainer.removeChild(searchResultsItemsContainer.firstChild);
+  }
+
+  if (searchQuery.trim() !== '') {
+    let hasResults = false; // Track if there are any search results
+
+    items.forEach(function(item) {
+      let itemText = item.textContent.toLowerCase();
+      if (itemText.indexOf(searchQuery) !== -1) {
+        const listItem = document.createElement('li');
+        listItem.textContent = itemText.replace('➕', '');
+        searchResultsItemsContainer.appendChild(listItem);
+        hasResults = true;
+      }
+    });
+
+    if (hasResults) {
+      searchResultsContainer.style.display = 'block';
+    } else {
+      searchResultsContainer.style.display = 'none';
+    }
+  } else {
+    searchResultsContainer.style.display = 'none';
+  }
+});
+
+// Copy button in Search Results container
+const copySearchButton = document.getElementById('copy-search-button');
+copySearchButton.addEventListener('click', function() {
+  const searchResultsList = document.querySelectorAll('#search-results-items li');
+  const searchResultsText = Array.from(searchResultsList).map(item => item.textContent).join('\n');
+  navigator.clipboard.writeText(searchResultsText)
+    .then(() => {
+      alert('Search results copied to clipboard');
+    })
+    .catch(err => {
+      console.error('Could not copy search results: ', err);
+    });
+});
+
+// Clear button in Search Results container
+const clearSearchButton = document.getElementById('clear-search-button');
+clearSearchButton.addEventListener('click', function() {
+  const searchInput = document.getElementById('search-input');
+  searchInput.value = ''; // Clear the search input text
+
+  const searchResultsContainer = document.getElementById('search-results');
+  const searchResultsItemsContainer = document.getElementById('search-results-items');
+  searchResultsItemsContainer.innerHTML = '';
+  searchResultsContainer.style.display = 'none';
+});
+
+
+// ==========================
+// Shopping list functions
+// ==========================
+
 let shoppingList = [];
 
-// Copy the shopping list items to the clipboard
 function copyToClipboard() {
   const copyText = shoppingList.map(item => `- ${item}`).join('\n');
   navigator.clipboard.writeText(copyText)
@@ -12,32 +82,27 @@ function copyToClipboard() {
     });
 }
 
-// Clear all the items in the shopping list
 function clearShoppingList() {
   shoppingList.length = 0; // This will clear the array
   displayShoppingList(); // Update the displayed shopping list
   hideShoppingList(); // Hide the shopping list
 }
 
-// Show the shopping list container
 function showShoppingListContainer() {
   const container = document.getElementById('shopping-list');
   container.style.display = 'block';
 }
 
-// Hide the shopping list container
 function hideShoppingListContainer() {
   const container = document.getElementById('shopping-list');
   container.style.display = 'none';
 }
 
-// Call this function when you want to show the shopping list
 function showShoppingList() {
   displayShoppingList();
   showShoppingListContainer();
 }
 
-// Call this function when you want to hide the shopping list
 function hideShoppingList() {
   hideShoppingListContainer();
 }
@@ -88,46 +153,38 @@ function displayShoppingList() {
   }
 
   if (shoppingList.length > 0) {
-    // Create and append the title
-    // const title = document.createElement('h2');
-    // title.textContent = 'Shopping List';
-    // shoppingListItemsContainer.appendChild(title);
-
-    // Create the item list
     const itemList = document.createElement('ul');
     itemList.className = 'item-list';
 
-    // Append each item to the list
     shoppingList.forEach(item => {
-    const listItem = document.createElement('li');
+      const listItem = document.createElement('li');
 
-    const itemText = document.createElement('span');
-    itemText.className = 'item-text'; // Add a class to your span
-    itemText.textContent = item;
-    listItem.appendChild(itemText);
+      const itemText = document.createElement('span');
+      itemText.className = 'item-text';
+      itemText.textContent = item;
+      listItem.appendChild(itemText);
 
-    const removeButton = document.createElement('button');
-    removeButton.textContent = '  ➖';
-    removeButton.style.color = "#dc3545";
-    removeButton.onclick = () => {
-      toggleInShoppingList(item);
-    };
+      const removeButton = document.createElement('button');
+      removeButton.textContent = '  ➖';
+      removeButton.style.color = "#dc3545";
+      removeButton.onclick = () => {
+        toggleInShoppingList(item);
+      };
 
-    listItem.appendChild(removeButton);
-    itemList.appendChild(listItem);
-  });
+      listItem.appendChild(removeButton);
+      itemList.appendChild(listItem);
+    });
 
-    // Append the item list to the shopping list container
     shoppingListItemsContainer.appendChild(itemList);
-
-    // Show the shopping list container
     shoppingListContainer.style.display = 'block';
   } else {
-    // Hide the shopping list container
     shoppingListContainer.style.display = 'none';
   }
 }
 
+// ====================================================
+// Menu handling functions for text-menu and photo-menu
+// ====================================================
 
 function loadTextMenu(language) {
   const textMenuPane = document.getElementById('text-menu');
@@ -200,7 +257,6 @@ function loadTextMenu(language) {
 }
 
 
-/* Do we need this? */
 var languageDropdown = document.getElementById('language');
 if (languageDropdown) {
   var language = languageDropdown.value;
@@ -259,6 +315,10 @@ function loadMenuCards(language) {
     });
 }
 
+// ==========================
+// Language handling functions
+// ==========================
+
 function loadLanguage(language) {
   var elements = document.querySelectorAll('.lang-en, .lang-te');
   elements.forEach(function(element) {
@@ -274,8 +334,14 @@ function loadLanguage(language) {
   loadMenuCards(language);
 }
 
+// Other language handling functions...
+
+// ==========================
+// DOMContentLoaded event
+// ==========================
+
 window.addEventListener('DOMContentLoaded', function() {
-  loadShoppingList(); // Load the shopping list from local storage when the page loads
+  loadShoppingList();
   var selectedLanguage = localStorage.getItem('selectedLanguage') || 'en';
 
   var languageDropdown = document.getElementById('language');
@@ -287,11 +353,9 @@ window.addEventListener('DOMContentLoaded', function() {
       loadLanguage(this.value);
     };
 
-    // Get the buttons by their ID
     const copyButton = document.getElementById('copy-button');
     const clearButton = document.getElementById('clear-button');
 
-    // Assign the appropriate function to each button's click event
     copyButton.addEventListener('click', copyToClipboard);
     clearButton.addEventListener('click', clearShoppingList);
   } else {
